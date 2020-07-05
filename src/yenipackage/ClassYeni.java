5f5eb29339f0735c0cb82825ddd0aaa834eeb6ea
Serhat Ay <seraksa18@gmail.com>
2020-07-05 22:25:03 +0200

    son deneme

--- src/yenipackage/ClassYeni.java
+++ src/yenipackage/ClassYeni.java
@@ -5,6 +5,7 @@ public class ClassYeni {
 	public static void main(String[] args) {
 		System.out.println("ilk commit maserden");
 		System.out.println("ilk commit maserden");
+		System.out.println("bir deneme daha");
 
 	}
 
